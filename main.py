f0a0aed58d22c4e6b526536452102901f4ea24d9
fernando meraz <fermerazz12@gmail.com>
2025-06-23 01:25:44 -0600

    added the verbose option that prints the output and tokens of the response & user's input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,12 +9,20 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 
+
 if len(sys.argv) < 2:
     print("Missing an argument after the name of the file, program is about to close")
     sys.exit(1)
 else:
-    response = client.models._generate_content(model='gemini-2.0-flash-001',
-                                           contents=sys.argv[1])
-    print(response.text)
-    print(f"""Prompt tokens: {response.usage_metadata.prompt_token_count}
-    Response tokens: {response.usage_metadata.candidates_token_count} """)
+    user_prompt = sys.argv[1]
+    is_verbose = "--verbose" in sys.argv
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    response = client.models.generate_content(model='gemini-2.0-flash-001',
+                                           contents=messages)
+    if is_verbose == True:
+        print(response.text)
+        print(f"""User prompt: {user_prompt} \nPrompt tokens: {response.usage_metadata.prompt_token_count} \nResponse tokens: {response.usage_metadata.candidates_token_count}""")
+    else:
+        print(response.text)
