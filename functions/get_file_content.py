2200d0427f4a8228fc174ea029fad5c0d75aeb67
fernando meraz <fermerazz12@gmail.com>
2025-06-24 22:32:20 -0600

    Finished the file logic

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -4,7 +4,18 @@ def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)    
     target_file = os.path.abspath(os.path.join(working_directory, file_path))
     if os.path.commonpath([abs_working_dir, target_file]) != abs_working_dir:
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        return f'Error: Cannot read "{target_file}" as it is outside the permitted working directory'
     
-    if not os.path.isfile(file_path):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
+    if not os.path.isfile(target_file):
+        return f'Error: File not found or is not a regular file: "{target_file}"'
+    try:
+        MAX_CHARS = 10000
+        with open(target_file, "r") as f:
+            file_content_str = f.read(MAX_CHARS + 1)
+            if len(file_content_str) > MAX_CHARS:
+                truncated_content = file_content_str[:MAX_CHARS] + f'[...File "{target_file}" truncated at 10000 characters]'
+                return truncated_content
+            else:
+                return file_content_str
+    except Exception as e:
+        return f"Error: {str(e)}"
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 # from subdirectory.filename import function_name
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print(get_files_info("calculator", "calculator"))
-print(get_files_info("calculator", "calculator/pkg"))
-print(get_files_info("calculator", "/bin"))
-print(get_files_info("calculator", "../"))
+print(get_file_content("calculator", "main.py"))
+print(get_file_content("calculator", "pkg/calculator.py"))
+print(get_file_content("calculator", "/bin/cat"))
