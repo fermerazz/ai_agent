df576ef8434db70489440cf244a083e8e07af90e
fermerazz <fermerazz12@gmail.com>
2025-06-29 16:52:59 -0600

    Finished the logic behind the write_file function

--- functions/write_file.py
+++ functions/write_file.py
@@ -7,8 +7,12 @@ def write_file(working_directory, file_path, content):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     
     try:
-        if not os.path.exists(file_path):
-            dir = os.dirname(target_file)
+        target_file_dir = os.path.dirname(target_file)
+        os.makedirs(target_file_dir, exist_ok=True)
+
+        with open(target_file, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
 
 
     except Exception as e:
