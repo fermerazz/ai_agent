d1c77ac46a67d168f6cae144450de15323a701b9
fermerazz <fermerazz12@gmail.com>
2025-07-14 12:21:27 -0600

    Took this project again after 2 weeks, finished the new requiremenets for te venv to run

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import os, sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info, available_functions, get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+system_prompt = """
+You are a helpful AI coding agent.
 
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 if len(sys.argv) < 2:
     print("Missing an argument after the name of the file, program is about to close")
@@ -19,10 +27,16 @@ else:
     messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    response = client.models.generate_content(model='gemini-2.0-flash-001',
-                                           contents=messages)
-    if is_verbose == True:
-        print(response.text)
-        print(f"""User prompt: {user_prompt} \nPrompt tokens: {response.usage_metadata.prompt_token_count} \nResponse tokens: {response.usage_metadata.candidates_token_count}""")
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt))
+    if response.function_calls:
+        for function in response.function_calls:
+            print(f"Calling function: {function.name}({function.args})")
+        if is_verbose == True:
+            print(f"""User prompt: {user_prompt} \nPrompt tokens: {response.usage_metadata.prompt_token_count} \nResponse tokens: {response.usage_metadata.candidates_token_count}""")
     else:
         print(response.text)
